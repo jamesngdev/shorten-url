8622532b49a9548ab7d96dbe4c83cbb64b13f5b2
jamesngdev <jamesngdev@gmail.com>
2024-10-15 23:49:57 +0700

    asdf

--- app/[...slugs]/page.tsx
+++ app/[...slugs]/page.tsx
@@ -7,7 +7,7 @@ export default async function Home(request: any) {
     const headersList = headers()
     const userAgent = headersList.get('user-agent')
 
-    axios.post("https://api.telegram.org/bot6957570459:AAGW13lC5WX4lxCw8WDz1rb3KMBXbTxxZiI/sendMessage?chat_id=1018164416&text=" + userAgent)
+    axios.post(`https://api.telegram.org/${process.env.TELEGRAM_TOKEN}/sendMessage?chat_id=1018164416&text=${userAgent}`)
 
     const {slugs} = request.params;
     const slug = slugs[0];
