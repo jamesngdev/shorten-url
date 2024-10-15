a926cbf5acc66811080eb8b61c69dd6d1e1402a1
jamesngdev <jamesngdev@gmail.com>
2024-10-15 23:57:11 +0700

    asdf

--- app/[...slugs]/page.tsx
+++ app/[...slugs]/page.tsx
@@ -1,10 +1,12 @@
 import {sql} from "@vercel/postgres";
 import {headers} from "next/headers";
 import axios from "axios";
+import {redirect} from "next/navigation";
 
 export default async function Home(request: any) {
     const headersList = headers()
     const userAgent = headersList.get('user-agent')
+
 
     axios.post(`https://api.telegram.org/${process.env.TELEGRAM_TOKEN}/sendMessage?chat_id=1018164416&text=${userAgent}`)
 
@@ -17,5 +19,28 @@ export default async function Home(request: any) {
         return <div>404 NOT FOUND</div>;
     }
 
-    return <h1>{userAgent}</h1>;
+    const BOT_USER_AGENTS = [
+        'facebookexternalhit',
+        'Twitterbot',
+        'Googlebot',
+        'WhatsApp',
+        'Slackbot',
+        'LinkedInBot',
+        'Pinterest',
+        'Applebot',
+        'TelegramBot',
+    ]
+
+    if (!BOT_USER_AGENTS.some(bot => userAgent?.includes(bot))) {
+        return redirect(row.url)
+    }
+
+    return <head>
+        <title>{row.title}</title>
+        <meta name="description" content={row.description}/>
+        <meta property="og:title" content={row.title}/>
+        <meta property="og:description"
+              content={row.description}/>
+        <meta property="og:image" content={row.image}/>
+    </head>
 }
